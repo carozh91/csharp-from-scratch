c2221980ae5df872ac6a7094bbc06c91f84cb65f
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-09-14 11:12:50 -0500

    New Class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Name = "John";
+            Name = "John";
+            Console.WriteLine($"HelloWorld! {Name}");
+            Console.ReadLine();
+        }
+    }
+}
