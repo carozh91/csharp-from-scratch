eb50a788a10ffce1385127c95453a25ee79ce464
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-09-15 19:43:26 -0500

    New program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,13 +1,43 @@
-﻿namespace HelloWorld
+﻿using System.Net.Mail;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main()
+        {   
+            DateOnly dateConverted = new DateOnly();
+            string nameInput;
+            string birthdayInput;
+            Console.WriteLine("Hola bienvenido al calculador de años");
+            Console.WriteLine("Escribe tu nombre");
+            nameInput = Console.ReadLine();
+            Console.WriteLine($"Un gusto conocerte {nameInput}");
+            Console.WriteLine("Escribe tu fecha de nacimineto de formato dd/mm/yy: ");
+            birthdayInput = Console.ReadLine();
+            bool IsDateValid = DateOnly.TryParse(birthdayInput. out dateConverted);
+            if(IsDateValid == false) Console.WriteLine($"la fecha es inválida {birthdayInput}");
+            var person = new Person
+            {
+                Name = nameInput,
+                Birthday = dateConverted,
+                Age = DateTime.Now.Year- dateConverted.Year
+            };
+
+
+            Console.WriteLine($"Tu nombre {person.Name}");
+            Console.WriteLine($"Tu fecha de nacimineto {person.Birthday}");
+            Console.WriteLine($"Tu edad {person.Age}");
+
+        }
+
+    public class Person
         {
-            string Name = "John";
-            Name = "John";
-            Console.WriteLine($"HelloWorld! {Name}");
-            Console.ReadLine();
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public DateOnly Birthday { get; set; }
         }
     }
 }
